55225d4350cee690e65c24d794d35e7626a67fa0
Gabriel Selotto <gabriel.selotto@contractor.vytelle.com>
2025-11-26 18:34:24 -0300

    latest update for api-tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,23 @@
 module.exports = {
   projectId: '2qqngi',
   e2e: {
-    //baseUrl: 'http://localhost:3000', // Change this to your application's base URL
+    //baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/e2e.js',
     fixturesFolder: 'cypress/fixtures',
     specPattern: 'cypress/e2e/**/*.cy.js',
+    excludeSpecPattern: ['**/node_modules/**', '**/*.node', '**/node*/**'],
     video: false,
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        }
+      });
+      return config;
+    },
   },
-  env: {
-    // Add any environment variables here
-  },
+  env: {},
   reporter: 'spec',
-  retries: {
-    runMode: 2,
-    openMode: 0,
-  },
+  retries: { runMode: 2, openMode: 0 },
 };
